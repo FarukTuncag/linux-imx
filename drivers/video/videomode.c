15ec6f84f33430618f3c6627f1301b8d11894662
FarukTuncag <160103004+FarukTuncag@users.noreply.github.com>
2024-07-17 18:55:53 +0300

    Update videomode.c

--- drivers/video/videomode.c
+++ drivers/video/videomode.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- *ft56
+ *ft56*5
  * generic display timing functions
  *
  * Copyright (c) 2012 Steffen Trumtrar <s.trumtrar@pengutronix.de>, Pengutronix

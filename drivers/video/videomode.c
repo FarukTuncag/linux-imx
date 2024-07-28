3aa42944bed88c2b1962c510a793a5111a3c64ca
FarukTuncag <160103004+FarukTuncag@users.noreply.github.com>
2024-07-28 13:41:22 +0300

    Update videomode.c

--- drivers/video/videomode.c
+++ drivers/video/videomode.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Deneme FT56
+ * Deneme FT56-v1
  * generic display timing functions
  *
  * Copyright (c) 2012 Steffen Trumtrar <s.trumtrar@pengutronix.de>, Pengutronix
